7debd70e456f77e05d4ef5192c81797f6aeee3f5
MunJunHo <mjh1995713@gmail.com>
2023-09-19 11:54:23 +0900

    타입변환 2

--- CPP_Study/42_TypeCasting/TypeCasting01.cpp
+++ CPP_Study/42_TypeCasting/TypeCasting01.cpp
@@ -14,15 +14,33 @@ public:
 class Dog
 {
 public:
+	Dog()// 아까전의 오류는 타입변환생성자를 없애주거나, 기본생성자를 만들어주면 해결됨.
+	{
+
+	}
+
 	// 타입 변환 생성자
 	Dog(const Knight& knight)
 	{
 		_age = knight._hp;
 	}
 
+	// 타입 변환 연산자 - return type이 없음
+	operator Knight()
+	{
+		return (Knight)(*this);
+	}
+
 public:
 	int _age = 1;
 	int _cuteness = 2;
+};
+
+
+class BullDog : public Dog
+{
+public:
+	bool _french;// 프렌치 불독
 };
 
 int main()
@@ -97,9 +115,65 @@ int main()
 	// [1] 연관없는 클래스 사이의 '값 타입' 변환
 	// 특징) 일반적으로 안됨 (예외 : 타입 변환 생성자, 타입 변환 연산자)
 	{
+		// 타입 변환 생성자
 		Knight knight;
 		Dog dog = (Dog)knight;
+
+		Knight knight2 = dog;// 타입 변환 연산자
 	}
 
+	// [2] 연관없는 클래스 사이의 참조 타입 변환
+	// 특징) 명시적으로는 ok
+	{
+		Knight knight;
+		// 어셈블리 : 포인터 = 참조
+
+		// [ 주소 ] -> [ Dog ]
+		Dog& dog = (Dog&)knight;
+		dog._cuteness = 12;// 이렇게 하면 건드리면 안될 메모리를 건들수 있다.
+
+	}
+
+	
+	
+	
+	// ------------------ 상속 관계에 있는 클래스 사이의 변환  ---------------------
+	// 특징) 자식 -> 부모 ok    /	부모 -> 자식 no
+	// [1] 상속 관계 클래스의 값 타입 변환
+	
+	{
+		//Dog dog;
+		//BullDog bulldog = (BullDog)dog;
+
+		BullDog bulldog;
+		Dog dog = bulldog;// _french 정보를 빼고 나머지 정보가 들어감.
+
+	}
+	
+	// [2] 상속 관계 클래스의 참조 타입 변환
+	// 특징) 자식 -> 부모 ok	/	부모-> 자식(암시적 no) (명시적 ok)
+	{
+		Dog dog;
+
+		//BullDog& bulldog = dog; - 통과 x
+		//BullDog& bulldog = (BullDog&)dog;  // - 통과 o
+
+		BullDog bulldog;
+		Dog& dog = bulldog;
+
+	}
+	
+	// 결론)
+	// [값 타입 변환] : 진짜 비트열도 바꾸고~ 논리적으로 말이 되게 바꾸는 변환
+	// - 논리적으로 말이 된다? (ex. BullDog -> Dog) ok
+	// - 논리적으로 말이 안 된다(ex Dog -> BullDog, Dog -> Knight) 안됨
+	// [참조 타입 변환] : 비트열은 냅두고 우리의 '관점'만 바꾸는 변환
+	// - 땡깡 부리면(명시적 요구) 해주긴 하는데, 말 안 해도 '그냥' (암시적)으로 해주는지는 안전성 여부와 연관이 있음
+	// -- 안전하다? (ex. BullDog -> Dog&) '그냥' (암시적으로) ok
+	// -- 위험하다? (ex. Dog -> BullDog&)
+	// --- 메모리 침범 위험이 있는 경우는 '그냥' (암시적으로) 해주진 않음 (위험하니까)
+	// --- 명시적으로 정말 정말 하겠다고 최종 서명을 하면 OK
+	
+	
 	return 0;
 }
