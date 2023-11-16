338b0b83bcf1440ebee437208a97b3118008c2ab
MunJunHo <mjh1995713@gmail.com>
2023-11-16 19:06:22 +0900

    splite 함수 구현

---
+++ CPP_Study/89_Splite/splite.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<string> split(string input, string delimeter)
+{
+	vector<string> ret;
+	long long pos = 0;
+	string token = "";
+	while ((pos = input.find(delimeter)) != string::npos)
+	{
+		token = input.substr(0, pos);
+		ret.push_back(token);
+		input.erase(0, pos + delimeter.length());
+	}
+	ret.push_back(input);
+	return ret;
+}
+
+int main()
+{
+	string s = "안녕하세요 홍길동은 사원입니다. 입사 2년차 입니다.!", d = " ";// d는 delimeter
+	vector<string> a = split(s, d);
+	for (string b : a)cout << b << endl;
+
+}
